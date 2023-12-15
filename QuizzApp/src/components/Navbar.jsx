2d0b3129057788d65ca49e4d3ccda04df7a11ee9
Melvin Kannan <139456150+Melvin0070@users.noreply.github.com>
2023-12-15 09:46:02 +0530

    Update Navbar.jsx

--- QuizzApp/src/components/Navbar.jsx
+++ QuizzApp/src/components/Navbar.jsx
@@ -6,6 +6,8 @@ function Navbar(props) {
   return (
     <nav>
       <h1 className='Title' style={{color: props.theme ? 'black' : 'whitesmoke'}}>Quizz App</h1>
+      
+      {/* To toggle between dark theme and lgiht theme */}
       <input type="checkbox" class="theme-checkbox" onClick={props.onThemeChange}></input>
     </nav>
   )
